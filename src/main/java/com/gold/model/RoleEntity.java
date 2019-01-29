517d3ddec4ca6deee7ad36d6925e70b9de20f5f2
AsteriksA <mr.a.galstyan@gmail>
2019-01-29 20:00:01 +0200

    Fixed method add() in the BookService

---
+++ src/main/java/com/gold/model/RoleEntity.java
@@ -0,0 +1,30 @@
+package com.gold.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "roleEntities")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String role;
+
+//    for example: ADMIN, USER, ANONYMOUS, MODERATOR
+    @ManyToMany(mappedBy = "roleEntities")
+    private Set<UserEntity> userEntities;
+
+}
